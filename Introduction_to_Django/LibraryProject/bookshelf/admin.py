27182a319ed2e28b739e54b44cdcf50f49df7e6b
jeyleekrave-dev <jeyleekrane@gmail.com>
2025-08-31 12:27:45 +0100

    bookshelf

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Book
 
+
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+# Register the Book model with the custom admin class.
+admin.site.register(Book, BookAdmin)
+
+
 # Register your models here.
 admin.site.register(Book)
