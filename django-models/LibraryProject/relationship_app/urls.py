6e8e197f42397c681e7aea24f9512c412fa04c24
jeyleekrave-dev <jeyleekrane@gmail.com>
2025-09-13 12:01:24 +0100

    logout url

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(),
          name='library_detail'),
-    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(
         template_name='relationship_app/logout.html'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+
     path('register/', views.register, name='register'),
 ]
