0c3f9f2e5917a81181dff2f1ae36ae76567455aa
jeyleekrave-dev <jeyleekrane@gmail.com>
2025-09-13 12:22:20 +0100

    added a patter to recreate this process

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(
+        template_name='relationship_app/logout.html'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('register/', views.register, name='register'),
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(),
          name='library_detail'),
-    path('logout/', LogoutView.as_view(
-        template_name='relationship_app/logout.html'), name='logout'),
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('register/', views.register, name='register'),
+
 ]
