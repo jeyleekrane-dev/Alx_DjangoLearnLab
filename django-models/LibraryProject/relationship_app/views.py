dc6d4ef08b8565c28be390790265b9de189072ae
jeyleekrave-dev <jeyleekrane@gmail.com>
2025-09-13 12:28:14 +0100

    aded views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,41 @@
 from .models import Library
-from .models import Book
-from django.shortcuts import render, get_object_or_404
-from django.views.generic.detail import DetailView
-from django.contrib.auth import views as auth_views
-from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import redirect
 from django.contrib.auth import login as auth_login
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import views as auth_views
+from django.views.generic.detail import DetailView
+from django.shortcuts import render, get_object_or_404
+from .models import Book
+from django.contrib.auth.decorators import user_passes_test
+from .models import UserProfile
+
+
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
 
 # User registration view
 
