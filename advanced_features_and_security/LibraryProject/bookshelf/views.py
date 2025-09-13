984415f1f485f4fc397daa74119951cadad974e7
jeyleekrave-dev <jeyleekrane@gmail.com>
2025-09-13 13:24:06 +0100

    advance user permissions with a vuewset

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,7 @@
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+	books = Book.objects.all()
+	return render(request, 'bookshelf/book_list.html', {'books': books})
 from django.contrib.auth.decorators import permission_required
 from .models import Book
 from django.shortcuts import get_object_or_404, redirect, render
