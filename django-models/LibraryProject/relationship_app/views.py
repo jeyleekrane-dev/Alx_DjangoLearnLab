60bf0ea511683ef5d5e55777776aa41289214511
jeyleekrave-dev <jeyleekrane@gmail.com>
2025-09-13 11:48:24 +0100

    authentications

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,26 @@
 from .models import Library
-from django.views.generic.detail import DetailView
-
-from django.shortcuts import render, get_object_or_404
-
 from .models import Book
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.detail import DetailView
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
+from django.contrib.auth import login as auth_login
+
+# User registration view
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('list_books')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
 
 # Function-based view to list all books
 
