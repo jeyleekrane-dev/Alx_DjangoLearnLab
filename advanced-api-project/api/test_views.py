09e9a1cb8570ac77b9488c9084c38dcd557cacb7
jeyleekrave-dev <jeyleekrane@gmail.com>
2025-09-28 14:23:53 +0100

    Test_view was added

---
+++ advanced-api-project/api/test_views.py
@@ -0,0 +1,92 @@
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth.models import User
+from .models import Author, Book
+
+
+class BookAPITestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.author1 = Author.objects.create(name='Author One')
+        self.author2 = Author.objects.create(name='Author Two')
+        self.book1 = Book.objects.create(title='Book One', publication_year=2020, author=self.author1)
+        self.book2 = Book.objects.create(title='Book Two', publication_year=2021, author=self.author2)
+        self.book3 = Book.objects.create(title='Another Book', publication_year=2020, author=self.author1)
+
+    def test_list_books(self):
+        response = self.client.get('/api/books/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 3)
+
+    def test_filter_books_by_title(self):
+        response = self.client.get('/api/books/?title=Book One')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['title'], 'Book One')
+
+    def test_filter_books_by_author_name(self):
+        response = self.client.get('/api/books/?author__name=Author One')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+    def test_filter_books_by_publication_year(self):
+        response = self.client.get('/api/books/?publication_year=2020')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+    def test_search_books(self):
+        response = self.client.get('/api/books/?search=Author One')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+    def test_order_books_by_title_asc(self):
+        response = self.client.get('/api/books/?ordering=title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        titles = [book['title'] for book in response.data]
+        self.assertEqual(titles, ['Another Book', 'Book One', 'Book Two'])
+
+    def test_order_books_by_publication_year_desc(self):
+        response = self.client.get('/api/books/?ordering=-publication_year')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        years = [book['publication_year'] for book in response.data]
+        self.assertEqual(years, [2021, 2020, 2020])
+
+    def test_create_book_authenticated(self):
+        self.client.force_authenticate(user=self.user)
+        data = {'title': 'New Book', 'publication_year': 2022, 'author': self.author1.id}
+        response = self.client.post('/api/books/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Book.objects.count(), 4)
+
+    def test_create_book_unauthenticated(self):
+        data = {'title': 'New Book', 'publication_year': 2022, 'author': self.author1.id}
+        response = self.client.post('/api/books/', data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_retrieve_book(self):
+        response = self.client.get(f'/api/books/{self.book1.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], 'Book One')
+
+    def test_update_book_authenticated(self):
+        self.client.force_authenticate(user=self.user)
+        data = {'title': 'Updated Book', 'publication_year': 2020, 'author': self.author1.id}
+        response = self.client.put(f'/api/books/{self.book1.id}/', data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.book1.refresh_from_db()
+        self.assertEqual(self.book1.title, 'Updated Book')
+
+    def test_update_book_unauthenticated(self):
+        data = {'title': 'Updated Book', 'publication_year': 2020, 'author': self.author1.id}
+        response = self.client.put(f'/api/books/{self.book1.id}/', data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_delete_book_authenticated(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.delete(f'/api/books/{self.book1.id}/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Book.objects.count(), 2)
+
+    def test_delete_book_unauthenticated(self):
+        response = self.client.delete(f'/api/books/{self.book1.id}/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
