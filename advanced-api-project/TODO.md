5b5b7a9d837e7a1bc923d25dadd990cb8d858526
jeyleekrave-dev <jeyleekrane@gmail.com>
2025-09-28 14:11:04 +0100

    filtering

--- advanced-api-project/TODO.md
+++ advanced-api-project/TODO.md
@@ -1,4 +1,4 @@
-g# Implement Generic Views for Book CRUD
+# Implement Generic Views for Book CRUD
 
 ## Steps to Complete:
 
@@ -17,3 +17,19 @@ g# Implement Generic Views for Book CRUD
 
 ### Step 4: Implement Permissions
 - [x] Add permission classes to all views
+
+# Enhance API with Filtering, Searching, Ordering
+
+## Steps to Complete:
+
+### Step 1: Set Up Filtering
+- [x] Add DjangoFilterBackend and filterset_fields to ListView
+
+### Step 2: Implement Search Functionality
+- [x] Add SearchFilter and search_fields to ListView
+
+### Step 3: Configure Ordering
+- [x] Add OrderingFilter and ordering_fields to ListView
+
+### Step 4: Update API Views
+- [x] Modify ListView with all filter_backends
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
 
@@ -25,6 +27,10 @@ class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['title', 'author__name', 'publication_year']
+    search_fields = ['title', 'author__name']
+    ordering_fields = ['title', 'publication_year']
 
 class UpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
