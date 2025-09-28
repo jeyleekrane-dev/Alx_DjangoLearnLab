a32d078722f1e43b42b046ac36a98384233554cb
jeyleekrave-dev <jeyleekrane@gmail.com>
2025-09-28 13:12:41 +0100

    modifeid models

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 
-class Author (models.Model):
-
+class Author(models.Model):
     name = models.CharField(max_length=255)
 
-    pass
-class Book (models.Model):
+   
+class Book(models.Model):
     title = models.CharField(max_length=255)
     publication_year = models.IntegerField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
-    
+
