989157975fb357ca57730e736a7dd3878fda8a3f
jeyleekrave-dev <jeyleekrane@gmail.com>
2025-09-28 13:45:18 +0100

    2nd project

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
+from .views import AuthorListCreateView,AuthorDetailView,BookListCreateView,BookDetailView
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    # path('api/', include('api.urls')),
+    path('books/', AuthorListCreateView.as_view(), name='author-list-create'),
+    path('books/<int:pk>/', AuthorDetailView.as_view(), name='author-detail'),
+    path('authors/', BookListCreateView.as_view(), name='book-list-create'),    
+    path('authors/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
 ]
